3c0cc8a1000dd60b8afeffec4d96251427903d1d
Mateusz Cyburt <mateusz.cyburt@schibsted.pl>
2017-12-18 13:19:34 +0100

    Initial Commit

---
+++ feedly-fresh/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  feedly-fresh
+//
+//  Created by matcybur on 18/12/2017.
+//  Copyright © 2017 matcybur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ feedly-fresh/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  feedly-fresh
+//
+//  Created by matcybur on 18/12/2017.
+//  Copyright © 2017 matcybur. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
